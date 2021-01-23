41b820eba754d25bafe72c382382a6b292062f31
5tack Trac3 <5tacktrac3@gmail.com>
2021-01-23 14:30:59 +0000

    changed port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http =  require('http');
 const hostname = 'localhost';
-const port = 3000;
+const port = process.env.PORT || 5000;
 
 const server = http.createServer( (req, res) => {
     res.statusCode = 200;
